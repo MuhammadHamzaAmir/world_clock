745ec7e0b10d3e604181a35fd8aabefdbbd551b7
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2022-12-15 21:10:51 +0500

    data passing

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,8 +1,8 @@
-import 'package:http/http.dart';
 import 'dart:convert';
 
-class WorldTime{
+import 'package:http/http.dart';
 
+class WorldTime {
   late String location;
   late String time;
   late String flag;
@@ -10,18 +10,23 @@ class WorldTime{
 
   WorldTime({required this.location, required this.flag, required this.url});
 
-
   Future<void> getTime() async {
+    try {
+      Map data =
+          await get(Uri.parse("http://worldtimeapi.org/api/timezone/$url"))
+              .then((response) {
+        return jsonDecode(response.body);
+      });
 
-    Map data = await get(Uri.parse("http://worldtimeapi.org/api/timezone/$url")).then((response) {
-      return jsonDecode(response.body);
-    });
+      String dateTime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
 
-    String dateTime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
-
-    DateTime now = DateTime.parse(dateTime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    time = now.toString();
+      DateTime now = DateTime.parse(dateTime);
+      now = now.add(Duration(hours: int.parse(offset)));
+      time = now.toString();
+    } catch (e) {
+      print('Caught error: $e');
+      time = 'Could not get time data';
+    }
   }
 }
